bc03e04dbabd2a58d0982f9fefcd3901e9854573
adasiek <90912558+adasiek193@users.noreply.github.com>
2022-06-22 11:39:13 +0200

    UPDATED ALL + SAVING THEME IS WORKING SHEEEEEESH

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -1,5 +1,28 @@
-const switchIcon = document.querySelector('.changetheme');
+const switchBtn = document.querySelector('.changetheme')
 
-switchIcon.addEventListener("click", () => {
-    document.querySelector('body').classList.toggle('light');
-})
+const toggleTheme = () => {
+    if (document.body.classList.contains('dark-mode')) {
+        document.body.classList.remove('dark-mode');
+        document.body.classList.add('light');
+        localStorage.setItem('dark-mode', 'false');
+    } else {
+        localStorage.setItem('dark-mode', 'true');
+        document.body.classList.add('dark-mode');
+        document.body.classList.remove('light');
+    }
+};
+
+const setThemeOnLaunch = () => {
+    const savedTheme = localStorage.getItem('dark-mode');
+    if (savedTheme === 'true') {
+        document.body.classList.add('dark-mode');
+        document.body.classList.remove('light');
+    } else if (savedTheme === 'false') {
+        document.body.classList.remove('dark-mode');
+        document.body.classList.add('light');
+    }
+};
+
+switchBtn.addEventListener('click', toggleTheme);
+
+setThemeOnLaunch();
